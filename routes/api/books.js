0bd88aa4b6a77a0eaa80dc168b1aff2a7f20e184
kate <katy.madden99@gmail.com>
2021-05-21 09:16:47 +0100

    fixed issue w books api

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const bookController = require("../../controllers/bookController");
 
-// Matches with "/api/books"
-router.route("/")
-  .get(bookController.findAll)
-  .post(bookController.create);
-
 // Matches with "/api/books/:id"
 router
   .route("/:id")
   .get(bookController.findById)
   .put(bookController.update)
   .delete(bookController.remove);
+
+// Matches with "/api/books"
+router.route("/")
+  .get(bookController.findAll)
+  .post(bookController.create);
 
 module.exports = router;
