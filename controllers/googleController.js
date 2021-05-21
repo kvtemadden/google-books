7ca06c33e397cfe1ff8f1eb4a10959a57728ed0e
kate <katy.madden99@gmail.com>
2021-05-21 09:18:50 +0100

    fixed controller axios call

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,18 +1,10 @@
 const axios = require("axios");
 const db = require("../models");
 
-// Defining methods for the googleController
-
-// findAll searches the Google Books API and returns only the entries we haven't already saved
-
-// It also makes sure that the books returned from the API all contain a title, author, link, description, and image
 module.exports = {
   findAll: function(req, res) {
     const { query: params } = req;
-    axios
-      .get("https://www.googleapis.com/books/v1/volumes", {
-        params
-      })
+    axios.get("https://www.googleapis.com/books/v1/volumes", { params })
       .then(results =>
         results.data.items.filter(
           result =>
